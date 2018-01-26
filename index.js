30e54d75526a221d054062b29e7e756ce2eae9e5
Bratok3000 <bratok3000@gmail.com>
2018-01-26 13:54:14 +0200

    JS essential lesson

---
+++ index.js
@@ -0,0 +1,5 @@
+var name = "Andrew"
+var time = "at 19:00"
+var place = "somewere in Lviv"
+var event = prompt("Insert event of CursorPlace: ", "has a lecture");
+console.log(name + " " + event + " " + time + " " + place);
