70b09e96e131f5cb1479e1bee4088b41dc54c295
Bratok3000 <Bratok3000@gmail.com>
2018-02-02 23:32:23 +0200

    last improvement

--- convertor.js
+++ convertor.js
@@ -2,40 +2,24 @@ var dollar = 28.92;
 var euro = 36.04;
 
 var numberOfDollars = +prompt("Input number of dollars", )
-var numberOfEuroes = prompt("Input number of euros", )
+while(isNaN(numberOfDollars) || numberOfDollars < 0.01 ){
+    alert("You are insert the incorect number. Please input number that are more then a 0 .");
+    numberOfDollars = parseFloat(prompt("Input number of dollars",))
+}
 
+var numberOfEuroes = prompt("Input number of euros", )
 numberOfEuroes = parseFloat(numberOfEuroes);
+while(isNaN(numberOfEuroes) || numberOfEuroes < 0.01 ){
+    alert("You are insert the incorect number. Please input number that are more then a 0 .");
+    numberOfEuroes = parseFloat(prompt("Input number of dollars",))
+}
 
 function convertorUSDtoUAH(){
-    while(isNaN(numberofDollars) || numberOfDollars < 0.01 ){
-        alert("You are insert the incorect number. Please input number that are more then a 0 .");
-        numberOfDollars = parseFloat(prompt("Input number of dollars",))
-    }
-    var USDtoUAH = dollar * numberOfDollars;
-    return USDtoUAH;
-    // if(!isNaN(numberOfDollars)){
-    // var USDtoUAH = dollar * numberOfDollars;
-    // return USDtoUAH;
-    // }
-    // else{
-    //     alert("number of dollars are not a number");
-    // }
+    return dollar * numberOfDollars;
 }
 
 function convertorEURtoUAH(){
-    while(isNaN(numberOfEuroes) || numberOfEuroes < 0.01 ){
-        alert("You are insert the incorect number. Please input number that are more then a 0 .");
-        numberOfEuroes = parseFloat(prompt("Input number of dollars",))
-    }
-    var EURtoUAH = dollar * numberOfEuroes;
-    return EURtoUAH;
-    // if(typeof(numberOfDollars)=="number"){
-    //     var EURtoUAH = euro * numberOfEuroes;
-    //     return EURtoUAH;
-    // }
-    // else{
-    //     alert("number of euroes are not a number");
-    // }
+    return euro * numberOfEuroes;
 }
 
 alert(numberOfEuroes + " euros are equal " + convertorEURtoUAH() + ", " 
